31d324827b5c03d46fabd542a028a41156a429da
orges <gesishabani1@gmail.com>
2019-09-01 16:16:27 +0200

    Correction

--- userbot/modules/stickers.py
+++ userbot/modules/stickers.py
@@ -234,7 +234,7 @@ async def kang(args):
                     await bot.send_read_acknowledge(conv.chat_id)
 
             await args.edit(
-                f"Uh nu, you commited a crime, hide [this](t.me/addstickers/{packname} from the police)",
+                f"Uh nu, you commited a crime, hide [this](t.me/addstickers/{packname}) from the police",
                 parse_mode='md')
 
 
